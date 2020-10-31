e5038b77a0048bdc586c4fb975af643bfff41daa
simphotonics <git@simphotonics.com>
2020-10-31 16:41:19 +0000

    Added test for triangular cdf.

--- test/density_functions_test.dart
+++ test/density_functions_test.dart
@@ -8,7 +8,7 @@ import 'package:statistics/statistics.dart';
 Type reflectType<T>() => T;
 
 void main() {
-  group('Uniform dist.', () {
+  group('uniformPdf.', () {
     final min = -2.0;
     final max = 3.0;
     num pdf(num x) => uniformPdf(x, min, max);
@@ -21,7 +21,7 @@ void main() {
       expect(integrate(pdf, min, max), 1.0);
     });
   });
-  group('Cumulative uniform dist.', () {
+  group('uniformCdf', () {
     final min = -2.0;
     final max = 3.0;
     num pdf(num x) => uniformPdf(x, min, max);
@@ -34,22 +34,22 @@ void main() {
     });
   });
 
-  group('Exp. dist.', () {
+  group('expPdf.', () {
     final mean = 2.5;
     num pdf(num x) => expPdf(x, mean);
     test('Shape', () {
       expect(pdf(0), 1 / mean);
       expect(pdf(100 * mean), 0);
     });
-    test('Value', () {
+    test('value', () {
       expect(pdf(math.log(2) * mean), 0.5 / mean);
     });
-    test('Normalization', () {
+    test('is normalized', () {
       expect(integrate(pdf, 0, 10 * mean, dxMax: 0.1), 1.0, precision: 1.0e-4);
     });
   });
 
-  group('Exp. cumulative dist.', () {
+  group('expCdf', () {
     final mean = 2.5;
     test('Limits', () {
       expect(expCdf(0.0, mean), 0);
@@ -66,7 +66,7 @@ void main() {
     });
   });
 
-  group('normalCdf', () {
+  group('stdNormalCdf', () {
     test('0 for small argument', () {
       expect(stdNormalCdf(-100.0), 0.0);
     });
@@ -81,7 +81,7 @@ void main() {
     });
   });
 
-  group('truncatedNormalPdf()', () {
+  group('truncatedNormalPdf', () {
     final min = -5;
     final max = 5;
     final mean = 1;
@@ -124,6 +124,22 @@ void main() {
     });
   });
 
+  group('truncatedNormalCdf', () {
+    final min = -4;
+    final max = 4;
+    final mean = 0;
+    final stdDev = 0.5;
+    test('0 for small argument', () {
+      expect(truncatedNormalCdf(min, min, max, mean, stdDev), 0.0);
+    });
+    test('1 for large argument', () {
+      expect(truncatedNormalCdf(max, min, max, mean, stdDev), 1.0);
+    });
+    test('y(0) = 0.5', () {
+      expect(truncatedNormalCdf(mean, min, max, mean, stdDev), 0.5);
+    });
+  });
+
   group('Triangular pdf', () {
     final min = 4.0;
     final max = 10.0;
@@ -132,22 +148,36 @@ void main() {
       expect(triangularPdf(min, min, max), 0);
       expect(triangularPdf(max, min, max), 0);
       expect(
-          triangularPdf(
-            min + h,
-            min,
-            max,
-          ),
-          triangularPdf(
-            max - h,
-            min,
-            max,
-          ));
-
+        triangularPdf(min + h, min, max),
+        triangularPdf(max - h, min, max),
+      );
       expect(triangularPdf(min + h, min, max), 1 / h);
       expect(triangularPdf(max - h + 1e-12, min, max), 1 / h);
     });
     test('Is normalized', () {
-      expect(integrate((x) => triangularPdf(x, min, max), min, max), 1.0);
+      expect(
+          integrate((x) => triangularPdf(x, min, max), min - 1, max + 1), 1.0);
+    });
+  });
+
+  group('Triangular cdf', () {
+    final min = 4.0;
+    final max = 10.0;
+    final h = (max - min) / 2;
+    test('left limit', () {
+      expect(triangularCdf(4.0, min, max), 0);
+    });
+    test('right limit', () {
+      expect(triangularCdf(10.0, min, max), 1.0);
+    });
+    test('value', () {
+      expect(triangularCdf(min + h, min, max), 0.5);
+    });
+    test('equal to integrated pdf', () {
+      expect(integrate((x) => triangularPdf(x, min, max), min, min + 1.0),
+          triangularCdf(min + 1.0, min, max));
+      expect(integrate((x) => triangularPdf(x, min, max), min, min + 2.1),
+          triangularCdf(min + 2.1, min, max));
     });
   });
 }
