e3848cc7a34dab67db1798af5ff46641bc125f03
simphotonics <git@simphotonics.com>
2020-10-31 13:58:40 +0000

    Export histogram parameters.

--- example/bin/sample_generator_example.dart
+++ example/bin/sample_generator_example.dart
@@ -1,20 +1,37 @@
+import 'dart:io';
+
 import 'package:statistics/statistics.dart';
 
-void main(List<String> args) {
-  final min = 1.0;
-  final max = 9.0;
-  final mean = 5.0;
-  final stdDev = 2.0;
-  final sample = generateTruncatedNormalSample(1000, min, max, mean, stdDev);
+void main(List<String> args) async {
+  final sampleSize = 3000;
+  final min = 1.5;
+  final max = 6.0;
+  final mean = 3.0;
+  final stdDev = 1.0;
+  final sample =
+      generateTruncatedNormalSample(sampleSize, min, max, mean, stdDev);
 
   final stats = SampleStatistics(sample);
-  print(stats.mean);
-  print(stats.stdDev);
-  print(stats.min);
 
-  sample.exportHistogram(
-    '../plots/truncated_normal.hist',
-    pdf: (x) =>
-        truncatedNormalPdf(x, stats.min, stats.max, stats.mean, stats.stdDev),
+  // Export histogram
+  await sample.exportHistogram(
+    '../plots/truncated_normal$sampleSize.hist',
+    pdf: (x) => truncatedNormalPdf(x, min, max, mean, stdDev),
   );
+
+  // Export variables
+  final file = File('../plots/truncated_normal$sampleSize.dat');
+
+  final b = StringBuffer();
+  b.writeln('# Histogram: Truncated normal distribution.');
+  b.writeln('# -----------------------------------------');
+  b.writeln('# Parameters used to generate the random sample:');
+  b.writeln('sampleSize = $sampleSize');
+  b.writeln('min = $min');
+  b.writeln('max = $max');
+  b.writeln('mean = $mean');
+  b.writeln('sampleMean = ${stats.mean}');
+  b.writeln('stdDev = $stdDev');
+
+  await file.writeAsString(b.toString());
 }
