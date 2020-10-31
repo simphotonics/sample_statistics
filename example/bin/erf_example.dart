1a77eed08065f33ff8e7fcc8a4596ca724fd9ab3
simphotonics <git@simphotonics.com>
2020-10-31 16:35:54 +0000

    Used sample size instead of hardcoding.

--- example/bin/erf_example.dart
+++ example/bin/erf_example.dart
@@ -5,8 +5,8 @@ void main(List<String> args) {
   final x1 = 10;
   final range = x1 - x0;
   final n = 1000;
-  final dx = range / 1000;
-  final y = List<num>.generate(1000, (i) => erf(x0 + i * dx));
+  final dx = range / n;
+  final y = List<num>.generate(n, (i) => erf(x0 + i * dx));
 
   y.export('example/plots/erf.dat', range: [x0, x1]);
 }
