5d18e8b0c2e95a10c16582ffdbb75cc5ba259ab3
simphotonics <git@simphotonics.com>
2020-11-05 17:24:06 +0000

    Added export statement.

--- lib/sample_statistics.dart
+++ lib/sample_statistics.dart
@@ -5,4 +5,5 @@ library sample_statistics;
 export 'src/base/density_functions.dart';
 export 'src/base/sample_generators.dart';
 export 'src/base/sample_stats.dart';
+export 'src/base/utility_extensions.dart';
 export 'src/base/utility_functions.dart';
