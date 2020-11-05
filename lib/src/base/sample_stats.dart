1284d25f88371eecc70366ed5da61f0bde92b88d
simphotonics <git@simphotonics.com>
2020-11-05 16:00:01 +0000

    Moved export and exportHistogram to  sep. ext.

--- lib/src/base/sample_stats.dart
+++ lib/src/base/sample_stats.dart
@@ -1,4 +1,3 @@
-import 'dart:io';
 import 'dart:math' as math;
 
 import 'package:exception_templates/exception_templates.dart';
@@ -117,12 +116,12 @@ class SampleStats {
   ///
   /// * The second list represent a count of how many sample values fall into
   ///   each interval.
-  ///    - If `normalize == true`, the list entries will be normalized using the
+  ///    - If `normalize == true`, the histogram count will be normalized using the
   ///     factor: `sampleSize * intervalSize`. This is suitable when comparing
   ///     the histogram to a probability distribution.
   /// * The third list is only added if `normalized == true`. It contains the
   ///   `probabilityDensity` evaluated at each point `(min, min + h, ..., max)`.
-  ///   The default value of `probabilityDensity` is `normalPdf(x, mean, stdDev)`,
+  ///   - The default value of `probabilityDensity` is `normalPdf(x, mean, stdDev)`,
   ///   where `mean` is the sample mean and `stdDev` is the sample standard
   ///   deviation.
   List<List<num>> histogram({
@@ -138,15 +137,12 @@ class SampleStats {
     final intervalSize = (max - min) / intervals;
     final n = intervals + 1;
     probabilityDensity ??= (num x) => normalPdf(x, mean, stdDev);
-
     // final minMid = min + 0.5 * intervalSize;
-
     final xValues = List<num>.generate(n, (i) => min + i * intervalSize);
     final yValues = List<num>.generate(n, (_) => 0.0);
-    final pdf = List<num>.generate(
-        n, (i) => probabilityDensity!(min + i * intervalSize));
 
     for (final current in sample) {
+      // Calculate interval index of current.
       final index = (current - min) ~/ intervalSize;
       ++yValues[index];
     }
@@ -155,6 +151,8 @@ class SampleStats {
       for (var i = 0; i < n; ++i) {
         yValues[i] = yValues[i] / (sampleSize * intervalSize);
       }
+      final pdf = List<num>.generate(
+          n, (i) => probabilityDensity!(min + i * intervalSize));
       return [xValues, yValues, pdf];
     } else {
       return [xValues, yValues];
@@ -171,7 +169,7 @@ class SampleStats {
     _stdDev?.updateCache();
   }
 
-  /// Throw an exception if `sample` is empty.
+  /// Throws an exception if `sample` is empty.
   static void assertNotEmpty(Iterable sample) {
     if (sample.isEmpty) {
       throw ExceptionOfType<EmptyIterable>(
@@ -182,13 +180,16 @@ class SampleStats {
   }
 }
 
+/// Extension on `List<num>` providing the
+/// method `removeOutliers`.
 extension StatisticsUtils on List<num> {
   /// Removes and returns all list entries
   /// with a value satisfying the condition:
   ///
   /// `(value < q1 - factor * iqr) || (value > q3 + factor * iqr)`
   ///
-  /// where `iqr = q3 - q1` is the inter-quartile range, `q1` is the first quartile,
+  /// where `iqr = q3 - q1` is the inter-quartile range,
+  /// `q1` is the first quartile,
   /// and `q3` is the third quartile.
   ///
   /// Note: The default value of `factor` is 1.5.
@@ -211,44 +212,86 @@ extension StatisticsUtils on List<num> {
     });
     return result;
   }
+}
 
-  /// Exports the entries of `this` to the file `filename`.
-  /// - Each entry is written to a separate row.
-  /// - Set `indexFirstColumn` to `true` to include an index.
-  Future<File> export(
-    String filename, {
+/// Extension on `List<num>` providing the methods
+/// `export` and `exportHistogram`.
+extension Export on List<num> {
+  /// Writes the entries of `this` to a `String`.
+  /// - Values are separated by `delimiter`.
+  /// - Each data record is written to a separate row.
+  /// - If `range` is not specified the first column will contain
+  ///  a data-record index.
+  ///
+  /// Usage:
+  /// ```
+  /// final dx = pi/100;
+  /// final y = List<num>.generate(101, (i) => sin(pi + i*dx) );
+  ///
+  /// final lookupTable = y.export(
+  ///   precision: 4,
+  ///   range: [pi, 2*pi],
+  ///   label: '// x  sin(x)',
+  ///
+  /// );
+  /// ```
+  /// The `String` tab has the following content:
+  /// ```Console
+  /// // x  sin(x)
+  /// 3.142 1.225e-16
+  /// 3.173 -0.03141
+  /// 3.204 -0.06279
+  /// ...
+  /// 6.221 -0.03141
+  /// 6.252 -2.449e-16
+  /// ```
+  String export({
     int precision = 20,
-    List<num> range = const [0, 1],
-    String label = '#     x                      y',
+    List<num>? range = const [0, 1],
+    String label = '#     x     y',
+    String delimiter = ' ',
   }) {
-    final file = File(filename);
     final b = StringBuffer();
     b.writeln(label);
-    final xRange = range.last - range.first;
-    final dx = xRange / length;
-    final x0 = range.first;
+
+    final x0 = (range == null)
+        ? 0
+        : (range.isEmpty)
+            ? 0
+            : range.first;
+    final dx = (range == null)
+        ? 1
+        : (range.length < 2)
+            ? 1
+            : (range.last - range.first) / length;
+
     for (var i = 0; i < length; ++i) {
-      b.writeln(
-          '${x0 + i * dx}       ${this[i].toStringAsPrecision(precision)}');
+      b.writeln('${(x0 + i * dx).toStringAsPrecision(precision)}'
+          '$delimiter${this[i].toStringAsPrecision(precision)}');
     }
-    return file.writeAsString(b.toString());
+    return b.toString();
   }
 
-  /// Builds a histogram and exports it to the file `filename`.
-  /// * Set `normalize` to true to normalize the total column area.
-  /// * The default number of histogram intervals (or bins)
-  ///   is the cubic root of the sample size.
-  Future<File> exportHistogram(
-    String filename, {
+  /// Builds a histogram from the entries of `this`
+  /// and writes the data records to a `String`.
+  /// * If `normalize == true` the histogram count will be normalized such
+  ///   that the total histogram area is equal to 1.0.
+  /// * If `normalize == true`, the function `pdf` is evaluated for
+  ///   each histogram interval and added as a third column.
+  /// * The parameter `pdf` defaults to:
+  ///   `(x) => normalPdf(x, sampleMean, sampleStdDev)`.
+  String exportHistogram({
     bool normalize = true,
     int? intervals,
     ProbabilityDensity? pdf,
+    verbose = false,
+    String commentCharacter = '#',
   }) {
-    final file = File(filename);
     final b = StringBuffer();
     if (length < 2) {
-      b.writeln('# Could not generate histogram. List is too short: $this');
-      return file.writeAsString(b.toString());
+      b.writeln('$commentCharacter Could not generate histogram. '
+          'List is too short: $this');
+      return b.toString();
     }
     final stats = SampleStats(this);
 
@@ -263,32 +306,33 @@ extension StatisticsUtils on List<num> {
 
     intervals ??= hist[0].length;
 
-    b.writeln('# Intervals: $intervals');
-    b.writeln('# Min: ${stats.min}');
-    b.writeln('# Max: ${stats.max}');
-    b.writeln('# Mean: ${stats.mean}');
-    b.writeln('# StdDev: ${stats.stdDev}');
-    b.writeln('# Median: ${stats.median}');
-    b.writeln('# First Quartile: ${stats.quartile1}');
-    b.writeln('# Third Quartile: ${stats.quartile3}');
-    b.writeln('# Interval size: $intervalSize');
-
-    b.writeln('# Integrated histogram: '
+    b.writeln('$commentCharacter Intervals: $intervals');
+    b.writeln('$commentCharacter Min: ${stats.min}');
+    b.writeln('$commentCharacter Max: ${stats.max}');
+    b.writeln('$commentCharacter Mean: ${stats.mean}');
+    b.writeln('$commentCharacter StdDev: ${stats.stdDev}');
+    b.writeln('$commentCharacter Median: ${stats.median}');
+    b.writeln('$commentCharacter First Quartile: ${stats.quartile1}');
+    b.writeln('$commentCharacter Third Quartile: ${stats.quartile3}');
+    b.writeln('$commentCharacter Interval size: $intervalSize');
+    b.writeln('$commentCharacter Integrated histogram: '
         '${hist[1].sum * intervalSize}');
-    b.writeln('#');
-    b.writeln(
-        '# -------------------------------------------------------------');
+    b.writeln('$commentCharacter');
+    b.writeln('$commentCharacter ------------------------------------'
+        '-------------------------');
     if (normalize) {
-      b.write(
-          '#      Range            ProbabilityDensity            Truncated Normal\n');
+      b.write('$commentCharacter     Range     Prob. Density     '
+          'Prob. Density Function\n');
     } else {
-      b.write('#      Range            Count\n');
+      b.write('$commentCharacter     Range     Count\n');
     }
-    print(b.toString());
+    if (verbose) {
+      print(b.toString());
+    }
     for (var i = 0; i < hist[0].length; ++i) {
       b.writeln(
-          '${hist[0][i]}      ${hist[1][i]}      ${normalize ? hist[2][i] : ''}');
+          '${hist[0][i]}     ${hist[1][i]}     ${normalize ? hist[2][i] : ''}');
     }
-    return file.writeAsString(b.toString());
+    return b.toString();
   }
 }
