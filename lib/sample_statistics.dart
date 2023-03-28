e34179c36108b83c304af7d7fd49b85d73c567e1
simphotonics <git@simphotonics.com>
2023-03-28 14:51:46 +0100

    Added libraries.

--- lib/sample_statistics.dart
+++ lib/sample_statistics.dart
@@ -1,9 +1,13 @@
 /// Utilities and functions for calculating sample statistics and generating
 /// random samples.
-library sample_statistics;
 
-export 'src/base/density_functions.dart';
-export 'src/base/sample_generators.dart';
-export 'src/base/sample_stats.dart';
-export 'src/base/utility_extensions.dart';
-export 'src/base/utility_functions.dart';
+export 'src/exceptions/invalid_function_parameter.dart';
+export 'src/extensions/export.dart';
+export 'src/extensions/histogram.dart';
+export 'src/extensions/integrate.dart';
+export 'src/extensions/root.dart';
+export 'src/samples/normal_random_sample.dart';
+export 'src/statistics/error_function.dart';
+export 'src/statistics/probability_density.dart';
+export 'src/statistics/sample_generators.dart';
+export 'src/statistics/stats.dart';
