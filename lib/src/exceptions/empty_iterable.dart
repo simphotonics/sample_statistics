53fb215d92759c6398aed63641b91e3e6b75fba2
simphotonics <git@simphotonics.com>
2023-03-28 14:47:31 +0100

    Renamed files.

--- lib/src/exceptions/empty_iterable.dart
+++
@@ -1,6 +0,0 @@
-import 'package:exception_templates/exception_templates.dart';
-
-/// Exception thrown if an iterable is unexpectedly empty.
-///
-/// For example when trying to return the min. value of an empty iterable.
-class EmptyIterable extends ExceptionType {}
