30afcdb41b189f3301ca30b2f807f0f2ae33c6da
simphotonics <git@simphotonics.com>
2023-03-28 14:52:09 +0100

    Renamed actions file.

--- tool/actions.sh
+++ tool/actions.sh
@@ -18,40 +18,34 @@ echo -e "${BLUE}=== Resolving dependencies $PWD...${RESET}"
 echo
 
 # Make sure .dart_tool/package_config.json exists.
-pub get
+dart pub get
 
 # Upgrade packages.
-pub upgrade
+dart pub upgrade
 
 echo
 echo -e "${PURPLE}=== Checking Source Code Formatting${RESET} $PWD..."
 echo
 # Overwrite files with formatted content: -w
 # Dry run: -n
-dartfmt -w $(find bin lib test -name \*.dart 2>/dev/null)
+dart format  bin lib test
 
 # Analyze dart files
 echo
 echo -e "${BLUE}=== Analyzing $PWD...${RESET}"
 echo
 
-dartanalyzer \
-    --enable-experiment=non-nullable \
+dart analyze \
     --fatal-warnings \
-    --fatal-infos \
-    --packages="$PWD/.packages" \
-    $(find bin lib test benchmark example -name \*.dart 2>/dev/null)
+    --fatal-infos
 
 # Running tests
 echo
 echo -e "${CYAN}=== Testing $PWD...${RESET}"
 echo
-#pub run test -r expanded --test-randomize-ordering-seed=random
-pub run --enable-experiment=non-nullable minimal_test:minimal_test.dart
-
-
+dart test -r expanded --test-randomize-ordering-seed=random
 
 echo
 echo -e "${BLUE}=== Running Examples $PWD/example ${RESET}"
 echo
-dart --enable-experiment=non-nullable example/bin/sample_statistics_example.dart
+dart  example/bin/sample_statistics_example.dart
