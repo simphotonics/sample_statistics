8315db85b451925b4d116afb6abfc88aad05b7a4
simphotonics <git@simphotonics.com>
2023-03-29 22:54:55 +0100

    Removed export statement.

--- lib/sample_statistics.dart
+++ lib/sample_statistics.dart
@@ -6,7 +6,6 @@ export 'src/extensions/export.dart';
 export 'src/extensions/histogram.dart';
 export 'src/extensions/integrate.dart';
 export 'src/extensions/root.dart';
-export 'src/samples/normal_random_sample.dart';
 export 'src/statistics/error_function.dart';
 export 'src/statistics/probability_density.dart';
 export 'src/statistics/sample_generators.dart';
